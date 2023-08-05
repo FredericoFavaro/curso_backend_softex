8361a96f48235980fa5fccccf906e58861a2f646
Frederico Fávaro <fredericofavaro@gmail.com>
2023-08-05 03:58:08 +0000

    Code Park 14

---
+++ 01-logica_programacao/code_park/14CP.js
@@ -0,0 +1,28 @@
+/*
+Desenvolva um programa que recebe do usuário nome completo e ano de nascimento que seja entre 1922 e 2021. A partir dessas informações, o sistema mostrará o nome do usuário e a idade que completou, ou completará, no ano atual (2022).
+
+Caso o usuário não digite um número ou apareça um inválido no campo do ano, o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.
+*/
+
+var readline = require("readline-sync");
+var nomeCompleto, anoNascimento, idade;
+function calculadoraIdade(){
+    while(true){
+        nomeCompleto = readline.question("Qual o seu nome? ");
+        anoNascimento = parseInt(readline.question("Em que ano você nasceu? "));
+        if (isNaN(anoNascimento) == false && anoNascimento >= 1922 && anoNascimento <= 2021) {
+            idade = 2022 - anoNascimento;
+            console.log(`${nomeCompleto}, em 2022, você tem/terá ${idade} anos`);
+            break;
+        } else {
+            throw new Error("Valor informado no ano de nascimento não é válido");
+        }
+    }
+}
+
+try {
+    calculadoraIdade()
+} catch(error){
+    console.log(error);
+    console.log("algo deu errado");
+}
