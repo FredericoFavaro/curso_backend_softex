843dcd6802c476e398bda26f4954a7cb2ba69ad9
Frederico Fávaro <fredericofavaro@gmail.com>
2023-08-13 06:02:32 +0000

    Code Park 18 - filas listas e pilhas

---
+++ 01-logica_programacao/code_park/18CP_filas_listas_pilhas.js
@@ -0,0 +1,51 @@
+/*
+Considere o array [3, 7, 9, 1, 0].
+
+O primeiro elemento inserido foi o 3, o segundo foi o 7 e assim por diante, seguindo a ordem.
+
+Levando em conta as três estruturas de dados (fila, lista e pilha) e as suas regras de operações, qual seria a sequência dos números removidos em cada uma delas para esvaziá-las completamente utilizando o array mencionado?
+
+### RESPOSTA ###
+Filas: Por seguir o principio First-In-First-Out, a remoção dos elementos ocorre pelo primeiro elemento incluido, ou seja, ele remove sempre o elemento do inicio da fila, no caso do array, primeiro será removido será o 3, depois o 7, depois o 9 e assim por diante
+
+Listas: Os elementos não são removidos em uma ordem específica. É necessário indicar que elemento deseja remover, assim o metodo indexOf() percorre o array em busca do elemento, se encontrado, ele é removido da lista.
+
+Pilhas: Seguem o principio oposto as filas, Last-In-First-Out. Logo, o elemento removido será sempre o ultimo que foi adicionado. No nosso array de exemplo, o primeiro a ser removido sera o 0, seguido pelo 1, depois pelo 9 e assim sucessivamente.
+*/
+
+let numeros = [3,7,9,1,0];
+
+// Removendo elementos na estrutura de dados fila
+function desenfileirar(){
+    if(estaVazia()){
+        return null;
+    }
+    return numeros.shift();
+}
+
+function estaVazia(){
+    return numeros.length === 0;
+}
+
+// Removendo elemendos na estrutura de dados lista
+function removerLista(elemento) {
+    const indice = numeros.indexOf(elemento);
+    if (indice !== -1){
+        numeros.splice(indice, 1);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Removendo elemendos na estrutura de dados pilha
+function desempilhar() {
+    if (estaVazia()) {
+        return null;
+    }
+    return numeros.pop();
+}
+
+function estaVazia(){
+    return numeros.length === 0;
+}
