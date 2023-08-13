e46d103426435e9081043695c155e36afd57bc45
Frederico Fávaro <fredericofavaro@gmail.com>
2023-08-13 06:12:13 +0000

    Ajustes

--- 01-logica_programacao/code_park/18CP_filas_listas_pilhas.js
+++ 01-logica_programacao/code_park/18CP_filas_listas_pilhas.js
@@ -5,12 +5,14 @@ O primeiro elemento inserido foi o 3, o segundo foi o 7 e assim por diante, segu
 
 Levando em conta as três estruturas de dados (fila, lista e pilha) e as suas regras de operações, qual seria a sequência dos números removidos em cada uma delas para esvaziá-las completamente utilizando o array mencionado?
 
-### RESPOSTA ###
-Filas: Por seguir o principio First-In-First-Out, a remoção dos elementos ocorre pelo primeiro elemento incluido, ou seja, ele remove sempre o elemento do inicio da fila, no caso do array, primeiro será removido será o 3, depois o 7, depois o 9 e assim por diante
+####################################
+###           RESPOSTA           ###
+####################################
+Filas: Por seguir o princípio First-In-First-Out, a remoção dos elementos ocorre pelo primeiro elemento incluído, ou seja, ele remove sempre o elemento do início da fila, no caso do array, primeiro será removido será o 3, depois o 7, depois o 9 e assim por diante
 
-Listas: Os elementos não são removidos em uma ordem específica. É necessário indicar que elemento deseja remover, assim o metodo indexOf() percorre o array em busca do elemento, se encontrado, ele é removido da lista.
+Listas: Os elementos não são removidos em uma ordem específica. É necessário indicar que elemento deseja remover, assim o método indexOf() percorre o array em busca do elemento, se encontrado, ele é removido da lista.
 
-Pilhas: Seguem o principio oposto as filas, Last-In-First-Out. Logo, o elemento removido será sempre o ultimo que foi adicionado. No nosso array de exemplo, o primeiro a ser removido sera o 0, seguido pelo 1, depois pelo 9 e assim sucessivamente.
+Pilhas: Seguem o princípio oposto as filas, Last-In-First-Out. Logo, o elemento removido será sempre o último que foi adicionado. No nosso array de exemplo, o primeiro a ser removido será o 0, seguido pelo 1, depois pelo 9 e assim sucessivamente.
 */
 
 let numeros = [3,7,9,1,0];
