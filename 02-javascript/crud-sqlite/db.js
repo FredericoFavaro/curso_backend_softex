d3e04910ec039e51b11487d0b8d0347f71168b8a
fredericofavaro <fredericofavaro@gmail.com>
2023-10-16 15:57:28 -0300

    Ajustes na atividade de conexao com banco de dados

--- 02-javascript/crud-sqlite/db.js
+++ 02-javascript/crud-sqlite/db.js
@@ -1,19 +1,8 @@
 const Sequelize = require("sequelize")
 
-//const sequelize = new Sequelize('sqlite::memory:') 
 const sequelize = new Sequelize('crud-sqlite', 'root', '123456', {
     storage: './database.sqlite',
     dialect: 'sqlite',
 });
-
-
-//(async () => {
-//  try {
-//    await sequelize.authenticate();
-//    console.log('Connection has been established successfully.');
-//  } catch (error) {
-//    console.error('Unable to connect to the database:', error);
-//  }
-//})();
 
 module.exports = sequelize;
---
+++ 02-javascript/trabalho/12-conexao-banco.js
@@ -0,0 +1,24 @@
+/*
+Realizando teste de conexao com banco de dados SQLite usando o Sequelize
+Pre-requisito: "npm i sequelize sqlite3", banco de dados deve estar instalado no sistema.
+Fontes: https://sequelize.org/docs/v6/getting-started/
+        https://sequelize.org/docs/v6/getting-started/#testing-the-connection 
+        https://www.luiztools.com.br/post/tutorial-de-crud-com-node-js-sequelize-e-sqlite/
+*/
+
+const Sequelize = require("sequelize")
+
+const sequelize = new Sequelize('crud-sqlite', 'root', '123456', {
+    storage: './database.sqlite',
+    dialect: 'sqlite',
+});
+
+// Para testar conexao, usar o metodo .authenticate(). Como prerequisito para funcionamento do "await", o codigo precisa se encapsulado com uma funcao IIFE: (async () => {codigo})();
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexão realizada com sucesso!!!');
+    } catch (error) {
+        console.error('Erro na conexão: ', error);
+    }
+})();
