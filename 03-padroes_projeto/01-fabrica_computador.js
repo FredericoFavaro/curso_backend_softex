b4e0d4d835b4e89831d62d5fabc3868cdc9efbb2
fredericofavaro <fredericofavaro@gmail.com>
2023-11-30 19:34:05 -0300

    Trabalho 01: Fabrica de computadores

---
+++ 03-padroes_projeto/01-fabrica_computador.js
@@ -0,0 +1,11 @@
+/*INSTRUCOES
+Considere que:
+- os produtos devem implementar uma interface comum;
+- o cliente deve utilizar apenas a fábrica para criar instancias do tipo abstrato da interface;
+- todo computador deve ter com getters, os atributos: ram, hdd, cpu e type;
+- ha dois tipos de computadores: pc e server:
+- ram e hd devem estar em GB;
+- CPU deve estar em GHz;
+- atraves do tipo informado, a fabrica decide qual tipo de computador irá instanciar;
+- quando o metodo .toString() for aplicado a um computador, ele deve imprimir seus atributos
+*/
